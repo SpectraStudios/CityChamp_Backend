e2196328c6626dec88f6ff50b8f30568af809ab8
pedrocruzio <pedro@rokailabs.com>
2024-03-21 20:48:11 -0400

    feat: scaffold frontend

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/pedrocruz/Work/2024/CityChamp/CityChamp_Backend/frontend/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
